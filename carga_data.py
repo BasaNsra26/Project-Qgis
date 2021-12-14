4fc7768dcb2fae02fb172966c9fdb55702972dd8
BasaNsra26 <91864011+BasaNsra26@users.noreply.github.com>
2021-12-14 12:18:56 -0500

    Update carga_data.py

--- carga_data.py
+++ carga_data.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 import pandas as pd
+import matplotlib.pyplot as plt 
   
 
 def cargar():
@@ -12,15 +13,46 @@ def cargar():
 									 key=None)
 		if archivo_cargado is not None:
 			df = pd.read_excel(archivo_cargado)
-			st.write(df.shape)
+			st.header("Análisis y exploración de datos")
+			st.write("El número de filas es: ",df.shape[0], "El numero de columnas es: ", df.shape[1])
 			st.write(df)
+			st.write("El análisis descriptivo por columna es")
 			st.write(df.describe())
+			st.write("Análisis de valores nulos en el marco de datos")
+			columna1, columna2 = st.columns(2)
+			with columna1:
+				 st.write(df.isnull().sum())
+			with columna2:
+				 st.bar_chart(df.isnull().sum(), width=600, height=400, use_container_width=True)
+			st.write("Análisis de la distribuición de datos")
+			seleccionar_columna = st.selectbox("Escoja una columna",options=list(df.columns))
+			fig, ax = plt.subplots()
+			ax.hist(df[seleccionar_columna], bins=20)
+			ax.grid()
+			ax.set_title("Histograma de {}".format(seleccionar_columna))
+			st.pyplot(fig)
+
 	elif tipo_archivo == ".csv":
 		archivo_cargado = st.sidebar.file_uploader("Importe aquí su archivo",
 									 type=[".csv"],
 									 key=None)
 		if archivo_cargado is not None:
 			df = pd.read_csv(archivo_cargado)
-			st.write(df.shape)
+			st.header("Análisis y exploración de datos")
+			st.write("El número de filas es: ",df.shape[0], "El numero de columnas es: ", df.shape[1])
 			st.write(df)
+			st.write("El análisis descriptivo por columna es")
 			st.write(df.describe())
+			st.write("Análisis de valores nulos en el marco de datos")
+			columna1, columna2 = st.columns(2)
+			with columna1:
+				 st.write(df.isnull().sum())
+			with columna2:
+				 st.bar_chart(df.isnull().sum(), width=600, height=400, use_container_width=True)
+			st.write("Análisis de la distribuición de datos")
+			seleccionar_columna = st.selectbox("Escoja una columna",options=list(df.columns))
+			fig, ax = plt.subplots()
+			ax.hist(df[seleccionar_columna], bins=20)
+			ax.grid()
+			ax.set_title("Histograma de {}".format(seleccionar_columna))
+			st.pyplot(fig)
